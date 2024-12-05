1ede787a947408f06bbde50f3e4236248a783cda
grovershyam <shyamgrover0014@gmail.com>
2024-12-05 16:13:48 +0530

    Fix xss vulnerability in AssignUserInputToInnerHtml.cs

--- IndividualChallenges/AssignUserInputToInnerHtml.cs
+++ IndividualChallenges/AssignUserInputToInnerHtml.cs
@@ -2,6 +2,6 @@ public void AssignUserInputToInnerHtml(string userInput)
 {
     // Insecure: Assigns raw user input to InnerHtml
     var res = new System.Web.UI.HtmlControls.HtmlGenericControl();
-    res.InnerHtml = userInput; // Vulnerable to XSS
+    res.InnerHtml = HttpUtility.HtmlEncode(userInput); // Encodes the input
     Console.WriteLine("Set InnerHtml to: " + userInput);
 }
