e462f5ff27f7d0c94f77358f4fd53aadbe3d72f0
Shubhraneel Pal <shubhraneelpal@gmail.com>
2024-12-05 16:16:52 +0530

    Update GetUserData.cs

--- IndividualChallenges/GetUserData.cs
+++ IndividualChallenges/GetUserData.cs
@@ -1,14 +1,15 @@
 public void GetUserData(int id)
 {
     // SQL injection vulnerability
-    string connectionString = "YourConnectionStringHere";
-    string query = $"SELECT * FROM Users WHERE id={id}";
+    string connectionString = Environment.GetEnvironmentVariable("Connection-String");
+    string query = "SELECT * FROM Users WHERE id=@id";
 
     using (var connection = new SqlConnection(connectionString))
     {
         connection.Open();
         using (var command = new SqlCommand(query, connection))
         {
+            command.Parameters.AddWithValue("@id", id); 
             using (var reader = command.ExecuteReader())
             {
                 while (reader.Read())
