533eb744a1e8ea370cbfa16e1a4150adb8e65348
Shubhraneel Pal <shubhraneelpal@gmail.com>
2024-12-05 16:22:11 +0530

    Update OpenFileStream.cs

--- IndividualChallenges/OpenFileStream.cs
+++ IndividualChallenges/OpenFileStream.cs
@@ -1,6 +1,12 @@
 public void OpenFileStream(string filePath)
 {
     // Opens a file using an unvalidated file path
+    if (filePath.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0) 
+    { 
+        throw new ArgumentException("Invalid file name."); 
+    } 
+    string filePath = Path.GetFullPath(filePath); 
+    
     using (var fs = new FileStream(filePath, FileMode.Open))
     {
         Console.WriteLine("File opened successfully: " + filePath);
