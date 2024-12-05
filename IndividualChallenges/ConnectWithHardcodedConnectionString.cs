5172d5b2ca312ab57aeca41ef241b019d2bf3026
Kamini Pandey <kpandey949@gmail.com>
2024-12-05 16:14:09 +0530

    Update ConnectWithHardcodedConnectionString.cs

--- IndividualChallenges/ConnectWithHardcodedConnectionString.cs
+++ IndividualChallenges/ConnectWithHardcodedConnectionString.cs
@@ -1,6 +1,6 @@
 public void ConnectWithHardcodedConnectionString()
 {
-    var connectionString = "Server=myServer;User Id=admin;Password=admin123;"; // Insecure: Hardcoded credentials
+    var connectionString = Environment.GetEnvironmentVariable("Connectionstring");; // Insecure: Hardcoded credentials
     Console.WriteLine("Connecting with hardcoded connection string.");
 
     // Simulate database connection
