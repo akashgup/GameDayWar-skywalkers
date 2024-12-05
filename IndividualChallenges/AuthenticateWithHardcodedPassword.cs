78e3e877997ec8da37fc3b95a88e600b8d377acf
Kamini Pandey <kpandey949@gmail.com>
2024-12-05 16:26:04 +0530

    Update AuthenticateWithHardcodedPassword.cs

--- IndividualChallenges/AuthenticateWithHardcodedPassword.cs
+++ IndividualChallenges/AuthenticateWithHardcodedPassword.cs
@@ -1,10 +1,10 @@
 public void AuthenticateWithHardcodedPassword()
 {
-    string password = "P@ssword!"; // Insecure: Hardcoded password
+    string password = Environment.GetEnvironmentVariable("PASSWORD"); // Insecure: Hardcoded password
     Console.WriteLine("Authenticating with hardcoded password.");
 
     // Simulate authentication
-    if (password == "P@ssword!")
+    if (password == "P@ssword!)
     {
         Console.WriteLine("Authentication successful.");
     }
